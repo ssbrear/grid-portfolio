7c462b85b207803ccff59046835338c0795f6390
Sebastian Brear <ssbrear@ucdavis.edu>
2021-04-11 13:40:23 -0700

    Modified settings to remove errors from console log in browser

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+      Hi
     </div>
   );
 }
 
 export default App;
+
+// EXAMPLE ROUTING FROM STONKS
+/* <Router>
+<Switch>
+  <Route path="/dashboard">
+    <Dashboard />
+  </Route>
+  <Route path="/">
+    <Home />
+  </Route>
+</Switch>
+</Router> */
